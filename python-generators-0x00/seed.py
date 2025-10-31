9dbb7afd2d857293174fae1a8464be9e466cfd98
Solomon <solomonf1227@gmail.com>
2025-10-31 17:32:28 +0300

    Add initial implementation of database functions in seed.py

---
+++ python-generators-0x00/seed.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This module contains functions to create a database, connect to it, """
+"""create a table, and insert data into the table using MySQL."""
+
+import mysql.connector
+
+
+
+def create_database(connection):
+    """- creates the database ALX_prodev if it does not exist"""
+
+
+def connect_to_prodev():
+    """connects the the ALX_prodev database in MYSQL"""
+
+
+def create_table(connection):
+    """creates a table user_data if it does not exists with the required fields"""
+
+
+def insert_data(connection, data):
+    """- inserts data in the database if it does not exist"""
