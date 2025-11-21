ed31e07f70a826a63ab02e182cdc073382493622
Solomon <solomonf1227@gmail.com>
2025-11-21 11:59:11 +0300

    Add additional test cases for access_nested_map function to handle exceptions

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,6 +17,18 @@ class TestAccessNestedMap(unittest.TestCase):
         """test Method"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Test that KeyError is raised for invalid paths."""
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(nested_map, path)
+
+        # Ensure KeyError message shows the missing key
+        self.assertEqual(str(e.exception), repr(path[-1]))
+
 
 if __name__ == '__main__':
     unittest.main()
