96f23b803f2180ec243dd81d0d7492b45a944f0d
Solomon <solomonf1227@gmail.com>
2025-11-25 18:30:40 +0300

    Add models for user roles, custom user, messages, and conversations

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,69 @@
+#!/usr/bin/env python3
+"""Models for the messaging app."""
+
+import uuid
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class UserRole(models.TextChoices):
+    """Enumeration for user roles."""
+    Guest = 'guest', 'Guest'
+    Host = 'host', 'Host'
+    Admin = 'admin', 'Admin'
+
+
+class CustomUser(AbstractUser):
+    """Custom user model extending AbstractUser."""
+    user_id = models.UUIDField(
+        primary_key = True,
+        default=uuid.uuid4(),
+        editable=False,
+        unique=True
+    )
+    first_name = models.CharField(max_length=50, blank=False)
+    last_name = models.CharField(max_length=50, blank=False)
+    email = models.EmailField(unique=True, blank=False, db_index=True)
+    password_hash = models.CharField(max_length=128, blank=False)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    role = models.CharField(
+        max_length=10,
+        choices=UserRole.choices,
+        default=UserRole.Guest,
+        blank=False
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Message(models.Model):
+    """Model representing a message sent by a user."""
+    message_id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        unique=True
+    )
+    sender_id = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='sent_messages'
+    )
+    message_body = models.TextField(blank=False)
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+
+class Conversation(models.Model):
+    """Model representing a conversation between users."""
+    conversation_id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        unique=True
+    )
+    participant_id = models.ManyToManyField(
+        CustomUser,
+        related_name='conversations',
+        on_delete=models.CASCADE
+        
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
