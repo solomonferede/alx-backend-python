52dbfe5a2682b71b75cc6cde59ae69dcc0bb007d
Solomon <solomonf1227@gmail.com>
2025-11-26 11:17:10 +0300

    Fix default value for user_id and update ForeignKey relationships in Message model

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -17,14 +17,13 @@ class CustomUser(AbstractUser):
     """Custom user model extending AbstractUser."""
     user_id = models.UUIDField(
         primary_key = True,
-        default=uuid.uuid4(),
+        default=uuid.uuid4,
         editable=False,
         unique=True
     )
     first_name = models.CharField(max_length=50, blank=False)
     last_name = models.CharField(max_length=50, blank=False)
     email = models.EmailField(unique=True, blank=False, db_index=True)
-    password_hash = models.CharField(max_length=128, blank=False)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     role = models.CharField(
         max_length=10,
@@ -43,7 +42,12 @@ class Message(models.Model):
         editable=False,
         unique=True
     )
-    sender_id = models.ForeignKey(
+    conversation = models.ForeignKey(
+        'Conversation',
+        on_delete=models.CASCADE,
+        related_name='messages'
+    )
+    sender = models.ForeignKey(
         CustomUser,
         on_delete=models.CASCADE,
         related_name='sent_messages'
@@ -62,8 +66,6 @@ class Conversation(models.Model):
     )
     participants = models.ManyToManyField(
         CustomUser,
-        related_name='conversations',
-        on_delete=models.CASCADE
-        
+        related_name='conversations',      
     )
     created_at = models.DateTimeField(auto_now_add=True)
