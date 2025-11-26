70586da39886dc56f6841605725c5436e48bad56
Solomon <solomonf1227@gmail.com>
2025-11-26 12:08:06 +0300

    Update import

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python3
+""" URLs for the messaging app chats."""
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
 from .views import ConversationViewSet, MessageViewSet
+from rest_framework import routers
 
-# Create a router and register our viewsets
-router = DefaultRouter()
+# Create a router and register the  viewsets
+router = routers.DefaultRouter()
 router.register(r"conversations", ConversationViewSet, basename="conversation")
 router.register(r"messages", MessageViewSet, basename="message")
 
